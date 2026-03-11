8c1dc05abd5c61294e7bd7af76ddaa01fdce4c1f
rladydwns2361 <gimyongjun796@gmail.com>
2026-03-11 16:04:06 +0900

    organization-templetes

---
+++ src/main/java/com/lms/Main.java
@@ -0,0 +1,13 @@
+package main.java.com.lms;
+
+public class Main {
+    public class Main {
+
+        public static void main(String[] args) {
+            System.out.println("LMS Project Started");
+
+            // 추후 LMS 기능 모듈 연결 예정
+            // 예: UserService, CourseService 등
+        }
+    }
+}

4307b1c4c97816e0171705d0a8b7edb194c60525
rladydwns2361 <gimyongjun796@gmail.com>
2026-03-18 01:20:05 +0900

    feat: 로그인 기능 구현(#15)

--- src/main/java/com/lms/model/dao/ProfessorDAO.java
+++ src/main/java/com/lms/model/dao/ProfessorDAO.java
@@ -1,4 +1,46 @@
 package com.lms.model.dao;
 
+import com.lms.common.JDBCTemplate;
+import com.lms.common.QueryUtil;
+import com.lms.model.dto.LoginRequestDTO;
+import com.lms.model.dto.LoginUserDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ProfessorDAO {
+
+    public LoginUserDTO loginProfessor(Connection con, LoginRequestDTO request) {
+
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+        LoginUserDTO loginUser = null;
+
+        String qeury = QueryUtil.getQuery("loginProfessor");
+
+        try {
+            pstmt = con.prepareStatement(qeury);
+            pstmt.setString(1, request.getUserId());
+            pstmt.setString(2, request.getPassword());
+
+            rset = pstmt.executeQuery();
+
+            if (rset.next()) {
+                loginUser = new LoginUserDTO();
+                loginUser.setRole("PROFESSOR");
+                loginUser.setUserId(rset.getString("professor_id"));
+                loginUser.setUserName(rset.getString("professor_name"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("교수 로그인 조회 실패", e);
+        } finally {
+            JDBCTemplate.close(rset);
+            JDBCTemplate.close(pstmt);
+        }
+
+        return loginUser;
+    }
 }
--- src/main/java/com/lms/model/service/AuthService.java
+++ src/main/java/com/lms/model/service/AuthService.java
@@ -1,4 +1,35 @@
 package com.lms.model.service;
 
+import com.lms.common.JDBCTemplate;
+import com.lms.model.dao.ProfessorDAO;
+import com.lms.model.dao.StudentDAO;
+import com.lms.model.dto.LoginRequestDTO;
+import com.lms.model.dto.LoginUserDTO;
+
+import java.sql.Connection;
+
 public class AuthService {
+
+    private final StudentDAO studentDAO = new StudentDAO();
+    private final ProfessorDAO professorDAO = new ProfessorDAO();
+
+    public LoginUserDTO login(LoginRequestDTO request) {
+
+        if(request == null){
+            return null;
+        }
+
+        Connection con = JDBCTemplate.getConnection();
+
+        try{
+            if ("STUDENT".equalsIgnoreCase(request.getRole())){
+                return studentDAO.loginStudent(con,request);
+            }else if ("PROFESSOR".equalsIgnoreCase(request.getRole())){
+                return professorDAO.loginProfessor(con,request);
+            }
+            return null;
+        }finally{
+            JDBCTemplate.close(con);
+        }
+    }
 }
